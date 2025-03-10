602d15e790626daf9a253635b7710580a3a5b96c
Amit Ben Yaakov <amitby98@gmail.com>
2025-03-10 10:06:03 +0200

    created comments model

--- server/src/models/Comments.model.ts
+++ server/src/models/Comments.model.ts
@@ -1,8 +1,35 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "./User.model";
+import { IPost } from "./Post.model";
 
 export interface IComment extends Document {
-    user: mongoose.Schema.Types.ObjectId | IUser;
-    post: mongoose.Schema.Types.ObjectId | IPost;
-    body: string;
-    createdAt: Date;
-    updatedAt: Date;
+  user: mongoose.Schema.Types.ObjectId | IUser;
+  post: mongoose.Schema.Types.ObjectId | IPost;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
+
+const CommentSchema: Schema = new Schema<IComment>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Post",
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IComment>("Comment", CommentSchema);
