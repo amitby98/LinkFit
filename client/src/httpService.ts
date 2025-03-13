24c1aac3cf4da7319bfca9167a4c3482dfb75eef
Amit Ben Yaakov <amitby98@gmail.com>
2025-03-13 07:38:37 +0200

    swagger setup

--- client/src/httpService.ts
+++ client/src/httpService.ts
@@ -7,7 +7,7 @@ export const httpService = axios.create({
 httpService.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers!.Authorization = `${token}`;
+    config.headers!.Authorization = `Bearer ${token}`;
   }
   return config;
 });
