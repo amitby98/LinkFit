2c0123b257da8c9e7157637dc5d9e3695e12e2f5
Natali Mizrahi <natalimizrahi22@gmail.com>
2025-03-14 22:11:43 +0200

    initialize jest

---
+++ server/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
+};
