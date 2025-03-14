fbb24b183da6e0b4af0eed6cb68aa04505635e9b
Natali Mizrahi <natalimizrahi22@gmail.com>
2025-03-14 22:13:52 +0200

    fixed some issues

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,21 +4,25 @@ dotenv.config();
 
 // Database connection
 const connectDB = async () => {
-  try {
-    const mongoUri = process.env.MONGO_URI;
-    if (!mongoUri) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
-    const conn = await mongoose.connect(mongoUri);
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      const mongoUri = process.env.MONGO_URI;
+      if (!mongoUri) {
+        throw new Error("MONGO_URI is not defined in environment variables");
+      }
+      const conn = await mongoose.connect(mongoUri);
+      resolve();
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(`Error: ${error.message}`);
-    } else {
-      console.error("Unknown error");
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`Error: ${error.message}`);
+      } else {
+        console.error("Unknown error");
+      }
+      reject(error);
+      process.exit(1);
     }
-    process.exit(1);
-  }
+  });
 };
 export default connectDB;
