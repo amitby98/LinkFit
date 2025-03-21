378a1d09e3aaff734a6c518c733bade21bd7413d
Amit Ben Yaakov <amitby98@gmail.com>
2025-03-21 16:08:34 +0200

    updated time of post

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,14 +1,17 @@
-import { format } from "date-fns";
+import { differenceInHours, format } from "date-fns";
 
 export function formatDate(timestamp: string) {
   const postDate = new Date(timestamp);
   const now = new Date();
   const diffInMinutes = Math.floor((now.getTime() - postDate.getTime()) / 60000);
+  const diffInHours = differenceInHours(now, postDate);
 
   if (diffInMinutes < 1) {
     return "Just now";
   } else if (diffInMinutes < 60) {
     return `${diffInMinutes} minutes ago`;
+  } else if (diffInHours < 24) {
+    return `${differenceInHours} hours ago`;
   } else {
     return format(postDate, "MMMM d, yyyy");
   }
